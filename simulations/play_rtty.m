7a0e39aee5b382a8a69828607a969fbcd6ef90fb
bbbales2 <bbbales2@0506805b-f2e6-16a2-31bd-fa486085329b>
2010-12-06 00:21:20 +0000

    I'm not really sure, just making sure the latest code is in the SVN

--- simulations/play_rtty.m
+++ simulations/play_rtty.m
@@ -1,5 +1,5 @@
 function play_rtty(message, sn)
-    signal = awgn(gen_rtty(8000, 1445, 1275, 1 / 45.45, 2, message), sn);
+    signal = awgn(gen_rtty(8000, 1275, 1455, 1 / 45.45, 2, message), sn);
     signal = signal / max(signal);
 
     soundsc(signal, 8000);
--- simulations/tdtl.m
+++ simulations/tdtl.m
@@ -1,29 +1,29 @@
 clear all; clc;
 
 % RTTY characteristics
-Fmark = 1275.0;
-Fspace = 1445.0;
+Fmark = 200.0;
+Fspace = 400.0;
 F0 = (Fmark + Fspace) / 2;
 
 Wmark = 2 * pi() * Fmark;
 Wspace = 2 * pi() * Fspace;
 
 %Generate input signal
-dt = 1 / 6900;
-sig_vals = gen_rtty(1 / dt, Fspace, Fmark, 1 / 70, 'Hello World');
+dt = 1 / 8000;
+sig_vals = gen_rtty(1 / dt, Fmark, Fspace, 1 / 70, 2, 'H');
 sig_time = (0 : length(sig_vals) - 1) * dt;
 runtime = sig_time(length(sig_time));
 
 % System characteristics
-psi = pi() / 3
+psi = pi() / 2;
 
 W0 = (Wmark + Wspace) / 2;
 T0 = 2 * pi() / W0;
 
 tau = psi / W0;
 
-W1 = Wmark / W0
-W2 = Wspace / W0
+W1 = Wmark / W0;
+W2 = Wspace / W0;
 
 K1 = 1;
 G1 = K1 / W0;
@@ -42,12 +42,12 @@ while t < runtime
     % Phase detector
     e = atan2(x, y);
     
-    e = x / y;
-    if(e > pi())
-        e = pi();
-    elseif(e < -pi())
-        e = -pi();
-    end
+    %e = x / y;
+    %if(e > pi())
+    %    e = pi();
+    %elseif(e < -pi())
+    %    e = -pi();
+    %end
     
     % Digital filter (put in a single pole?)
     c = G1 * e;
@@ -60,15 +60,17 @@ while t < runtime
     e_plot = [e_plot e];
 end
 
-%h = subplot(2, 1, 1);
-%hold off;
-%plot(0 : dt : runtime, interp1(sig_time, sig_vals, 0 : dt : runtime));
-%hold on;
-%stem(t_plot, interp1(sig_time, sig_vals, t_plot), '*r');
+h = subplot(2, 1, 1);
+plot(0 : dt : runtime, interp1(sig_time, sig_vals, 0 : dt : runtime));
+xlabel('Time (s)');
+ylabel('Signal');
+title('BFSK Signal');
+axis([0 runtime -1.1 1.1]);
 
-%subplot(2, 1, 2);
-plot(t_plot, e_plot);
+subplot(2, 1, 2);
+plot(t_plot, e_plot, 'ob-');
 xlabel('Time (s)');
 ylabel('Error Signal');
-title('Detection of 1275hz and 1445hz Signals');
+title('Detection of BFSK Signal');
+axis([0 runtime min(e_plot) * 1.1 max(e_plot) * 1.1]);
 
